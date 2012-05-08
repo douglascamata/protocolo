cb43000f91dacea4a1815d3ed00c0b569ada9b99
henrique2010 <henrique.passalini@gmail.com>
2012-05-07 23:46:31 -0300

    add js file and config for views

--- app/controllers/tramitacoes_controller.rb
+++ app/controllers/tramitacoes_controller.rb
@@ -1,3 +1,12 @@
 class TramitacoesController < InheritedResources::Base
   actions :new, :create, :show
+
+  def new
+    @tramitacao = Tramitacao.new
+    @requerimentos = Requerimento.filtrados
+  end
+
+  def create
+    create!(:notice => "Processos enviados.")
+  end
 end
--- app/models/tramitacao.rb
+++ app/models/tramitacao.rb
@@ -1,5 +1,5 @@
 class Tramitacao < ActiveRecord::Base
-  attr_accessible :setor_destino_id, :setor_origem_id, :requerimentos
+  attr_accessible :setor_destino_id, :setor_origem_id, :requerimento_ids
   
   belongs_to :setor_origem, class_name: 'Setor'
   belongs_to :setor_destino, class_name: 'Setor'
