942775b04e93349cbcb05d19f9a9c7fdc8edc912
Lucas Marques <lukspmarques@gmail.com>
2012-05-21 21:36:03 -0300

    (#4) Corrigindo spec acceptance de criação de despacho.

--- spec/acceptance/gerenciar_despacho_spec.rb
+++ spec/acceptance/gerenciar_despacho_spec.rb
@@ -3,16 +3,19 @@
 require 'spec_helper'
 
 feature 'gerenciar despacho' do
-  scenario 'visualização de despacho' do
+  scenario 'criação de despacho' do
     tipo_solicitacao = create(:tipo_solicitacao, descricao: 'tipo_1')
     processo = create(:processo, tipo_solicitacao: tipo_solicitacao)
+    
+    visit new_despacho_path
+    fill_in 'Conteudo', with: 'Conteudo do despacho'
+    select '00001/12', from: 'Processo'
+    click_button 'Enviar'
 
-    despacho = create(:despacho, conteudo: 'Conteudo do despacho.', processo: processo)
-
-    visit despacho_path(despacho)
+    page.should have_content 'Despacho adicionado'
     page.should have_content '00001/12'
     page.should have_content Date.today.strftime("%d/%m/%y")
     page.should have_content 'tipo_1'
-    page.should have_content 'Conteudo do despacho.'
+    page.should have_content 'Conteudo do despacho'
   end
 end
