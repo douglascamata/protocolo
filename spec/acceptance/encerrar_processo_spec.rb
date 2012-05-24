69ee599970438e50b250af6b0b6975e42134cb2b
henrique2010 <henrique.avulso@gmail.com>
2012-05-24 17:42:46 -0300

    (#8) add teste de aceitação para encerrar o processo

--- spec/acceptance/encerrar_processo_spec.rb
+++ spec/acceptance/encerrar_processo_spec.rb
@@ -7,6 +7,7 @@ feature "encerrar processo" do
     setor2 = create :setor, nome: 'Setor 2'
     processo = create :processo, setor_origem: setor1, destino_inicial: setor2
     no_ano(2012) { receber_processo_em(processo, setor2) }
+    create :motivo, nome: "motivo 1"
     
     visit encerrar_processos_path
 
@@ -14,9 +15,20 @@ feature "encerrar processo" do
     click_button 'Buscar'
 
     within_fieldset 'Processo' do
-      page.should have_content '00001/12'
+      page.should have_content 'Numero protocolo: 00001/12'
       page.should have_content 'Setor 1'
       page.should have_content 'Setor 2'
     end
+
+    within_fieldset 'Encerrar processo' do
+      page.should have_content 'Data de encerramento: ' + Date.today.strftime("%d/%m/%y")
+      page.should have_content 'Local de arquivamento: Setor 2'
+
+      select 'motivo 1', from: 'Motivo'
+      fill_in 'Observações', with: 'Obeservação qualquer'
+      click_button 'Encerrar'
+    end
+
+    page.should have_content 'Processo Encerrado.'
   end
 end
