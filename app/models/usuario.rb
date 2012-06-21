68f811eeed34be1ffa56c45381544810ce57ae04
henrique2010 <henrique.passalini@gmail.com>
2012-06-21 19:41:19 -0300

    mudança de "usuarios" para "users" (para não ter q fazer hack com cancan)

--- app/models/usuario.rb
+++
@@ -1,4 +0,0 @@
-class Usuario < ActiveRecord::Base
-  attr_accessible :papel
-  validates_presence_of :papel
-end
--- spec/models/usuario_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Usuario do
-  it{ should_not have_valid(:papel).when nil}      
-end
