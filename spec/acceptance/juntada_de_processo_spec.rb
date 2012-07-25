39368454da65e34614f2d74881baeed24bbd0d1d
henrique2010 <henrique.avulso@gmail.com>
2012-07-25 18:09:35 -0300

    Refatoração de teste (DRY)

--- spec/acceptance/juntada_de_processo_spec.rb
+++ spec/acceptance/juntada_de_processo_spec.rb
@@ -3,11 +3,13 @@
 require 'spec_helper'
 
 feature 'Juntada de processo' do
-  scenario 'Realizar juntada de processos', js: true do
-    processo_1 = create(:processo, numero_protocolo: '00001/12')
-    processo_2 = create(:processo, numero_protocolo: '00002/12')
-    processo_3 = create(:processo, numero_protocolo: '00003/12')
+  background do
+    @processo_1 = create(:processo, numero_protocolo: '00001/12')
+    @processo_2 = create(:processo, numero_protocolo: '00002/12')
+    @processo_3 = create(:processo, numero_protocolo: '00003/12')   
+  end
 
+  scenario 'Realizar juntada de processos', js: true do
     visit new_juntada_path
     fill_in 'processo_principal', with: '00001/12'
     click_button 'Buscar'
@@ -25,11 +27,7 @@ feature 'Juntada de processo' do
   end
 
   scenario 'Desapensar Processo de Juntada', js: true do
-    processo_1 = create :processo
-    processo_2 = create :processo
-    processo_3 = create :processo
-
-    create(:juntada, tipo: "Apensar", processo_principal: processo_1, processos: [processo_2, processo_3])
+    create(:juntada, tipo: "Apensar", processo_principal: @processo_1, processos: [@processo_2, @processo_3])
 
     visit desapensar_juntadas_path
 
@@ -42,10 +40,7 @@ feature 'Juntada de processo' do
   end
 
   scenario 'Desanexar Processo de Juntada', js: true do
-    processo_1 = create :processo
-    processo_2 = create :processo
-    processo_3 = create :processo
-    create(:juntada, tipo: "Anexar", processo_principal: processo_1, processos: [processo_2, processo_3])
+    create(:juntada, tipo: "Anexar", processo_principal: @processo_1, processos: [@processo_2, @processo_3])
 
     visit desanexar_juntadas_path
     select "00001/12", on: "Processo"
