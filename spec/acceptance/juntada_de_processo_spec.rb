00157db417c3dc04396e31cd6730eeccfe8fe622
Thawan Fidelis <thawan.fidelis@gmail.com>
2012-07-25 00:21:03 -0300

    Teste Aceitação para Desapensar processos (#6)

--- spec/acceptance/juntada_de_processo_spec.rb
+++ spec/acceptance/juntada_de_processo_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 feature 'Juntada de processo' do
-  scenario 'Realizar juntada de processos', javascript: true do
+  scenario 'Realizar juntada de processos', js: true do
     processo_1 = create(:processo, numero_protocolo: '00001/12')
     processo_2 = create(:processo, numero_protocolo: '00002/12')
     processo_3 = create(:processo, numero_protocolo: '00003/12')
@@ -23,5 +23,23 @@ feature 'Juntada de processo' do
     page.should have_content '00002/12'
     page.should have_content '00003/12'
   end
+
+  scenario 'Desapensar Processo de Juntada', js: true do
+    processo_1 = create :processo
+    processo_2 = create :processo
+    processo_3 = create :processo
+
+    create(:juntada, tipo: "Apensar", processo_principal: processo_1, processos: [processo_2, processo_3])
+
+    visit desapensar_juntadas_path
+
+    select "00001/12", on: "Processo"
+    uncheck '00002/12'
+    click_button 'Desapensar'
+
+    page.should have_content 'Processos desapensado com sucesso'
+    page.should_not have_content '00002/12'
+  end
+
 end
 
