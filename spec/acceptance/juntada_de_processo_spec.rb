842052aab77cfc4cd83807779c811ef535e4373c
Thawan Fidelis <thawan.fidelis@gmail.com>
2012-07-24 23:36:54 -0300

    Teste para juntada usando a busca (#5)

--- spec/acceptance/juntada_de_processo_spec.rb
+++ spec/acceptance/juntada_de_processo_spec.rb
@@ -3,17 +3,19 @@
 require 'spec_helper'
 
 feature 'Juntada de processo' do
-  scenario 'Realizar juntada de processos' do
+  scenario 'Realizar juntada de processos', javascript: true do
     processo_1 = create(:processo, numero_protocolo: '00001/12')
     processo_2 = create(:processo, numero_protocolo: '00002/12')
     processo_3 = create(:processo, numero_protocolo: '00003/12')
 
     visit new_juntada_path
+    fill_in 'processo_principal', with: '00001/12'
+    click_button 'Buscar'
     select 'Anexar', from: 'Tipo'
-    select '00001/12', from: 'Processo principal'
     check '00002/12'
     check '00003/12'
     click_button 'Salvar'
+
 
     page.should have_content "Juntada realizada com sucesso!"
     page.should have_content 'Anexar'
