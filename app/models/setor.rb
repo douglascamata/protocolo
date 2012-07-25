1e8a37406a2da4119e5f538381259caddbaa0793
Thawan Fidelis <thawan.fidelis@gmail.com>
2012-07-25 20:14:01 -0300

    Tornando matrícula e nome obrigatório para solicitante (#17)

--- app/models/setor.rb
+++ app/models/setor.rb
@@ -1,4 +1,5 @@
 class Setor < ActiveRecord::Base
   attr_accessible :nome
+  validates_presence_of :nome
 end
 
--- app/models/solicitante.rb
+++ app/models/solicitante.rb
@@ -1,4 +1,5 @@
 class Solicitante < ActiveRecord::Base
   attr_accessible :nome, :endereco, :telefone, :email, :matricula
+  validates_presence_of :nome, :matricula
 end
 
