8e4bcc5b2f182e2c71cd9961572b387442b85a3a
Thawan Fidelis <thawan.fidelis@gmail.com>
2012-07-25 00:49:34 -0300

    Javascript para selecionar juntada dinamicamente (#6)

--- app/assets/javascripts/juntadas.js
+++ app/assets/javascripts/juntadas.js
@@ -3,5 +3,11 @@ $(document).ready(function() {
     var numero_protocolo = $('input#numero_protocolo').attr("value");
     $.get('/juntadas/buscar?numero_protocolo=' + numero_protocolo);
   });
+
+  $('#processo').change(function() {
+    var id = this.value;
+    $.get('/juntadas/atualizar_processos?processo_id=' + id);
+  });
+
 });
 
