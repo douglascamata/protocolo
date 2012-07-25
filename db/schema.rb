7e57e4e1241b98f23233f10748516e064a3f290c
Thawan Fidelis <thawan.fidelis@gmail.com>
2012-07-25 19:58:30 -0300

    Novo schema (#17)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120621223833) do
+ActiveRecord::Schema.define(:version => 20120725215217) do
 
   create_table "despachos", :force => true do |t|
     t.text     "conteudo"
@@ -66,6 +66,10 @@ ActiveRecord::Schema.define(:version => 20120621223833) do
     t.string   "nome"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "endereco"
+    t.string   "telefone"
+    t.string   "email"
+    t.string   "matricula"
   end
 
   create_table "tipos_solicitacao", :force => true do |t|
@@ -101,11 +105,5 @@ ActiveRecord::Schema.define(:version => 20120621223833) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-  create_table "usuarios", :force => true do |t|
-    t.string   "papel"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
 end
