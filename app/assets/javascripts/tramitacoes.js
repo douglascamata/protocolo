aa7ee717c8df3d99ceaa389ed247a677ec9ef843
henrique2010 <henrique.avulso@gmail.com>
2012-07-25 17:53:05 -0300

    Refatoração na listagem de processor por setor (#20)

--- app/assets/javascripts/tramitacoes.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('#tramitacao_setor_origem_id').change(function() {
-    var id = this.value;
-    $.get('/tramitacoes/atualizar_processos?setor_id=' + id);
-  });
-});
