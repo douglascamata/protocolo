e9b4add5ee826c9cd0bd4e181635dc254b6268d8
henrique2010 <henrique.avulso@gmail.com>
2012-07-25 18:47:50 -0300

    Refatoração do js para Buscar Processos (#20)

--- app/assets/javascripts/buscar_processo.js
+++ app/assets/javascripts/buscar_processo.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
   
   $('button#buscar').click(function() {
     var numero_protocolo = $('input#numero_protocolo').attr("value");
-    $.get('/processos/buscar?numero_protocolo=' + numero_protocolo);
+    $.ajax($(this).data('url'), {
+      data: {numero_protocolo: numero_protocolo}
+    });
   });
 
 });
