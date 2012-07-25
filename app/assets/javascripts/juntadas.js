c632390fb56ff9c815025463c0689e6b1179cc77
henrique2010 <henrique.avulso@gmail.com>
2012-07-25 18:47:50 -0300

    Refatoração do js para Buscar Processos (#20)

--- app/assets/javascripts/juntadas.js
+++ app/assets/javascripts/juntadas.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-  $('button#buscar').click(function() {
-    var numero_protocolo = $('input#numero_protocolo').attr("value");
-    $.get('/juntadas/buscar?numero_protocolo=' + numero_protocolo);
-  });
+
 
   $('#processo').change(function() {
     var id = this.value;
