382c6e1e7bca51b05b6e74899e805d0224b50670
henrique2010 <henrique.passalini@gmail.com>
2012-08-29 19:39:09 -0300

    integração do Devise com Capybara de maneira certa!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,6 +50,7 @@ Spork.prefork do
   RSpec.configure do |config|
     config.include FactoryGirl::Syntax::Methods
     config.include Devise::TestHelpers, :type => :controller
+    config.after(:each) { Warden.test_reset! }
 
     # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
--- spec/support/user.rb
+++ spec/support/user.rb
@@ -1,6 +1,2 @@
-def logar(user)
-  visit new_user_session_path
-  fill_in 'Email', with: user.email
-  fill_in 'Password', with: user.password
-  click_button 'Sign in'
-end
+include Warden::Test::Helpers
+Warden.test_mode!
