0ac36a36711080e411eb6ca8a9a93b326fdddf40
henrique2010 <henrique.passalini@gmail.com>
2012-08-30 16:51:33 -0300

    (#15) autorização de solicitante p/ receber processo

--- spec/acceptance/recebimento_de_processo_spec.rb
+++ spec/acceptance/recebimento_de_processo_spec.rb
@@ -3,28 +3,31 @@
 require 'spec_helper'
 
 feature 'recebimento de processo' do
-  scenario 'autorização como admin' do
+  scenario 'autorização' do
     visit receber_processos_path
     current_path.should_not == receber_processos_path
     page.should have_content 'Você não tem permissão para acessar esse conteudo.'
-
-    login_as(create(:user, role: 'admin'), :scope => :user)
-    visit receber_processos_path
-    current_path.should == receber_processos_path
   end
 
   scenario 'padrão', js: true do
-    login_as(create(:user, role: 'admin'), :scope => :user)
-    
-    setor1 = create :setor, nome: 'Setor 1'
-    create :setor, nome: 'Setor 2'
-    no_ano(2012) { criar_processo_enviado_para(setor1) }
-    visit receber_processos_path
-    select 'Setor 1', from: 'Destino atual'
-    within_fieldset 'Processos não recebidos' do
-      page.should have_content '0001/12'
-      click_link 'Receber'
+    setor1 = create :setor
+    user_solicitante = create(:user_solicitante)
+    user_solicitante.solicitante.setores << setor1
+
+    [user_solicitante, create(:user_admin)].each do |user|
+      login_as(user, :scope => :user)
+      
+      no_ano(2012) { @processo = criar_processo_enviado_para(setor1) }
+      
+      visit receber_processos_path
+      select setor1.nome, from: 'Destino atual'
+      
+      within_fieldset 'Processos não recebidos' do
+        page.should have_content @processo.numero_protocolo
+        click_link 'Receber'
+      end
+
+      page.should have_content 'Processo ' + @processo.numero_protocolo + ' recebido com sucesso'
     end
-    page.should have_content 'Processo 00001/12 recebido com sucesso'
   end
 end
